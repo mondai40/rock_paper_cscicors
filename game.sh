2aacb7738df5d8b67c6a4c683859a3c360d9b9e3
mondai40 <tvwmdaimon40@gmail.com>
2019-07-17 00:13:17 -0700

    check hands function done

--- game.sh
+++ game.sh
@@ -1,54 +1,45 @@
 #!/bin/bash
-
-function checkHands() {
-    if [[ $1 -eq $2 ]]; then
-		echo "Draw"
-	elif [[$1 < ]]; then
-
-	else;
-
-	fi
-}
-
+source ./checkHands.txt
 
 declare -a hands=(Rock Paper Scisors)
 
 let com_num=$RANDOM%3
 com_hand=${hands[$com_num]}
-echo $com_hand
 
-
+echo ""
+echo "========================================="
 echo "Welcome to Rock Paper Scisors game!"
-
 PS3="Chose your hands from the above(1-3): "
 select hand in "${hands[@]}" "Quit"
 do
-	human_num=$REPLY
-	human_hand=${hands[$human_num - 1]}
-	case "$human_num" in
+	human_num=`expr $REPLY - 1`
+	human_hand=${hands[$human_num]}
+	case "$REPLY" in
 		1)
+			echo "========================================="
 			echo "Computer Hand: ${com_hand}"
 			echo "Your Hand: ${human_hand}"
 			checkHands $com_num $human_num
 			break;;
 		2)
+			echo "========================================="
 			echo "Computer Hand: ${com_hand}"
 			echo "Your Hand: ${human_hand}"
+			checkHands $com_num $human_num
 			break;;
 		3)
+			echo "========================================="
 			echo "Computer Hand: ${com_hand}"
 			echo "Your Hand: ${human_hand}"
+			checkHands $com_num $human_num
 			break;;
 		4)
+			echo "========================================="
 			echo "Thank you for playing, Bye!!"
 			break;;
 		*)
+			echo "========================================="
 			echo -e $'\e[1;31mWrong input\e[m';;
 	esac
 done
-
-function checkHands() {
-	if ($1 == $2); then
-		echo "Draw"
-	fi
-}
+echo ""
